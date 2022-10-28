95ee660676ff4b96f56ee31122dd1cdb0c491c37
HarryLB <harryloefflerbell@gmail.com>
2022-10-28 18:36:02 -0500

    I'm bad at git

--- BasicBinaryConvert.py
+++ BasicBinaryConvert.py
@@ -8,3 +8,24 @@ rootWindow.iconbitmap("BasicBinaryConverter\HarryICON1.ico")
 #Functions
 def toBinary():
     pass
+
+def toDecimal():
+    pass
+
+#Object Creation
+inputBox = Entry(rootWindow, width=50)
+toBinaryButton = Button(rootWindow,text= "Convert to Binary", command=toBinary)
+toDecimalButton = Button(rootWindow,text= "Convert to Binary", command=toDecimal)
+resultFrame = LabelFrame(rootWindow, padx=5, pady=5, text="Result", labelanchor= N)
+resultLabel = Label(resultFrame,text="")
+
+#Object Display
+inputBox.grid(row=0,column=0,columnspan=2,padx=10, pady=5)
+toBinaryButton.grid(row=1,column=0, pady=5)
+toDecimalButton.grid(row=1,column=1, pady=5)
+
+resultFrame.grid(row=2,column=0,columnspan=2,sticky=W+E, padx=10, pady=10)
+resultLabel.pack()
+
+
+rootWindow.mainloop()
